2b1761e7b5a2655dcc30dbf95616511ee25051dc
Amalendu Suresh <32508528+amalendusuresh@users.noreply.github.com>
2023-01-19 11:09:12 +0400

    Update contract.sol

--- Chapter 1/contract.sol
+++ Chapter 1/contract.sol
@@ -1,33 +1,39 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
-       // event
+     // event
      event NewZombie(uint zombieId, string name, uint dna);
    
+    // state Variables & Integers 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
+    // struct
     struct Zombie {
         string name;
         uint dna;
     }
-
+    
+    // array
     Zombie[] public zombies;
 
+    // private function declaration
     function _createZombie(string memory _name, uint _dna) private {
         // and fire it here
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        uint id = zombies.push(Zombie(_name, _dna)) - 1; // Working With Structs and Arrays
         emit NewZombie(id, _name, _dna);
     }
 
+    // view function
     function _generateRandomDna(string memory _str) private view returns (uint) {
-        uint rand = uint(keccak256(abi.encodePacked(_str)));
-        return rand % dnaModulus;
+        uint rand = uint(keccak256(abi.encodePacked(_str))); // keccak256 hash of abi.encodePacked(_str) and storing the result in rand
+        return rand % dnaModulus; //  return rand value modulus(%) dnaModulus.
     }
 
+    // public function
     function createRandomZombie(string memory _name) public {
-        uint randDna = _generateRandomDna(_name);
-        _createZombie(_name, randDna);
+        uint randDna = _generateRandomDna(_name); // run the _generateRandomDna function on _name, and store it in a uint named randDna
+        _createZombie(_name, randDna); // run the _createZombie function and pass it _name and randDna.
     }
 
 }
