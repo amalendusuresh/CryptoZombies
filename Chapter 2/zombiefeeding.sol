c1f6dfbbd5492af4bbab93c289e7e1c928c64d0b
Amalendu Suresh <32508528+amalendusuresh@users.noreply.github.com>
2023-01-23 12:14:04 +0400

    Update zombiefeeding.sol

--- Chapter 2/zombiefeeding.sol
+++ Chapter 2/zombiefeeding.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.5.0 <0.6.0;
 // Importing
 import "./zombiefactory.sol";
 
+// Interacting with other contracts(Interface)
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
@@ -21,30 +22,27 @@ contract KittyInterface {
 // Inheritance
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  // Initialize kittyContract here using `ckAddress` from above
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d; // initialized with ckAddress
+  KittyInterface kittyContract = KittyInterface(ckAddress); // KittyInterface named kittyContract
 
-  // Public function
+  // if statement added
   function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
     require(msg.sender == zombieToOwner[_zombieId]); // require statement to verify that msg.sender is equal to this zombie's owner
     Zombie storage myZombie = zombies[_zombieId]; // Declare a local Zombie named myZombie
     _targetDna = _targetDna % dnaModulus; // Zombie DNA 
     uint newDna = (myZombie.dna + _targetDna) / 2;
-
-
-    // Add an if statement here
-    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
-       newDna = newDna - newDna % 100 + 99;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) { // comparing the keccak256 hashes of _species and the string "kitty"
+       newDna = newDna - newDna % 100 + 99; // replace the last 2 digits of DNA with 99 (logic)
     }
-    _createZombie("NoName", newDna);
+    _createZombie("NoName", newDna); //  call _createZombie.
   }
-
+  
+  // Multiple Return Values
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
-    uint kittyDna;
-    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-
-    // And modify function call here:
-    feedAndMultiply(_zombieId, kittyDna, "kitty");
+    uint kittyDna; //  declare a uint named kittyDna. 
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId); //  kittyContract.getKitty function with _kittyId and store genes in kittyDna
+    feedAndMultiply(_zombieId, kittyDna, "kitty"); // function call feedAndMultiply
   }
 
 }
