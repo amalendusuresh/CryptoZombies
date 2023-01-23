9b35806e36ce8b5d32766a7181eeadd1e3518472
Amalendu Suresh <32508528+amalendusuresh@users.noreply.github.com>
2023-01-23 12:14:40 +0400

    Update zombiefeeding.sol

--- Chapter 2/zombiefeeding.sol
+++ Chapter 2/zombiefeeding.sol
@@ -22,7 +22,7 @@ contract KittyInterface {
 // Inheritance
 contract ZombieFeeding is ZombieFactory {
 
-  // Initialize kittyContract here using `ckAddress` from above
+  // Initialize kittyContract using `ckAddress` 
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d; // initialized with ckAddress
   KittyInterface kittyContract = KittyInterface(ckAddress); // KittyInterface named kittyContract
 
